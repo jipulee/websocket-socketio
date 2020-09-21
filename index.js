0097fb5b4af2292689ed77313132b53114090f51
khalil <jishen027@gmail.com>
2020-09-21 19:08:51 +0800

    upgrade sockets.io

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var express = require('express')
+var socket = require('socket.io')
 
 //App setup 
 var app = express()
-var server = app.listen(4000,function(){
+var server = app.listen(4000, () => {
   console.log('listener to require on port 4000')
-
 })
 
-//static files
+//Static files
 app.use(express.static('public'))
+
+//Socket setup
+var io = socket(server)
+
+//create a socket connection port 
+io.on('connection', (socket) => {
+  console.log('make socket connection', socket.id)
+
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data)
+  })
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data)
+  })
+})
 
